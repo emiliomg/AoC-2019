136037fd03216533f9342abff56ee1d3456fcf41
Emilio Markgraf <emilio.markgraf@fashionid.de>
2020-10-23 20:43:00 +0200

    Day 1 Star 1&2

--- build.sbt
+++ build.sbt
@@ -2,4 +2,4 @@ name := "AoC-2019"
 
 version := "0.1"
 
-scalaVersion := "2.13.1"
+scalaVersion := "2.13.3"
--- src/main/scala/de/emiliomg/adventofcode/y2019/day1/Day1.scala
+++ src/main/scala/de/emiliomg/adventofcode/y2019/day1/Day1.scala
@@ -1,41 +1,52 @@
 package de.emiliomg.adventofcode.y2019.day1
 
+import scala.annotation.tailrec
 import scala.io.Source
 
 /**
- * https://adventofcode.com/2019/day/1
- */
+  * https://adventofcode.com/2019/day/1
+  */
 object Day1 extends App {
 
-  val testData = parseData(getData("2019/1/test.txt"))
-  println(testData)
+  assert(firstStar(List(12)) == 2)
+  assert(firstStar(List(14)) == 2)
+  assert(firstStar(List(1969)) == 654)
+  assert(firstStar(List(100756)) == 33583)
 
-  //  println(firstStar(testData))
-  //  println(secondStar(testData))
+  assert(secondStar(List(14)) == 2)
+  assert(secondStar(List(1969)) == 966)
+  assert(secondStar(List(100756)) == 50346)
 
-  //  assert(firstStar(testData) == ???)
-  //  assert(secondStar(testData) == ???)
+  val data = parseData(getData("2019/1/input.txt"))
 
-  //  val data = parseData(getData("2018/1/input.txt"))
-  //
-  //  val firstStarResult = firstStar(data)
-  //  val secondStarResult = secondStar(data)
-  //
-  //  println(s"Result first star: $firstStarResult")
-  //  println(s"Result second star: $secondStarResult")
+  val firstStarResult  = firstStar(data)
+  val secondStarResult = secondStar(data)
 
-  //  assert(firstStarResult == ???)
-  //  assert(secondStarResult == ???)
+  println(s"Result first star: $firstStarResult")
+  println(s"Result second star: $secondStarResult")
 
-  //  def firstStar(data: ???) = {
-  //    ???
-  //  }
+  assert(firstStarResult == 3502510)
+  assert(secondStarResult == 5250885)
 
-  //  def secondStar(data: ???) = {
-  //    ???
-  //  }
+  def firstStar(data: List[Int]): Int =
+    data.map(calculateFuelForMass).sum
+
+  def secondStar(data: List[Int]): Int = {
+    @tailrec
+    def calculateFuel(newMass: Int, baseMass: Int): Double = {
+      calculateFuelForMass(newMass) match {
+        case newFuel if newFuel >= 0 => calculateFuel(newFuel, baseMass + newFuel)
+        case newFuel                 => baseMass
+      }
+    }
+    data.map(mass => calculateFuel(mass, 0))
+  }.sum.toInt
+
+  def calculateFuelForMass(mass: Int): Int = {
+    (Math.floor(mass / 3) - 2).toInt
+  }
 
   def getData(path: String): List[String] = Source.fromResource(path).getLines().toList
 
-  def parseData(data: List[String]): ??? = ???
+  def parseData(data: List[String]): List[Int] = data.map(_.toInt)
 }
